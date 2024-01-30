effa81e97a3b1c82da0d2c07d3580799390896cc
hyeong1 <mi75625@gmail.com>
2024-01-30 12:37:28 +0900

    fix: 사이트에서 변경된 url 처리

--- python-web-scraper.py
+++ python-web-scraper.py
@@ -9,56 +9,77 @@ all_skills = []
 
 # check pagenation
 def get_pages(url):
-    response = requests.get(url + "engineering/", headers = {'User-agent': 'your bot 0.1'})
+    response = requests.get(
+        url + "engineering/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
+    )
     soup = BeautifulSoup(response.content, "html.parser")
-    pages = len(soup.find("ul", class_="bsj-nav").find_all(class_="page-numbers")[0:-1])
+    pages = len(
+        soup.find("ul", class_="bsj-nav").find_all(class_="page-numbers")[0:-1])
     return pages
 
 # extract all skills
 def scrap_skills(url):
-    response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
+    response = requests.get(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
+    )
     soup = BeautifulSoup(response.content, "html.parser")
-    skills = soup.find("section").find("div", id="skills-list").find("ul").find_all("li")
+    skills = soup.find("section").find(
+        "div", id="skills-list").find("ul").find_all("li")
     for skill in skills:
         skill = skill.find("a").text.lower()
         pattern = re.compile(r"\s*(.+?)\s*\(.*\)$")
         skill = pattern.search(skill).group(1)
         skill = skill.replace(" ", "-")
-        if skill == "marketing":
-            skill = skill + "-2" # marketing url change
-        all_skills.append(skill)
+        skill = skill.replace("/", "")
+        if skill == "marketing" or skill == "management":
+            skill = skill + "-2"  # marketing url change
+        if not (skill == "engineering"):
+            all_skills.append(skill)
 scrap_skills(url)
 
 # extract jobs
 def scrap_page(url):
     print(url)
-    response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
-    soup = BeautifulSoup(response.content, "html.parser")
-    jobs = soup.find("ul", class_="jobs-list-items").find_all("li")
-
-    for job in jobs:
-        header = job.find("div", class_="bjs-jlid__header").find_all("a")
-        title = header[0].text
-        company = header[-1].text
-        desciption = job.find("div", class_="bjs-jlid__description").text
-        pattern = re.compile(r"\n\t\t\t(.+?)\t\t")
-        desciption = pattern.findall(desciption)
-        job_url = header[0]
-        if job_url:
-            job_url = job_url["href"]
-
-        job_data = {
-            "title": title,
-            "company": company,
-            "desciption": desciption,
-            "link": job_url,
+    response = requests.get(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         }
-        all_jobs.append(job_data)
+    )
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("ul", class_="jobs-list-items")
+    if jobs:
+        jobs = jobs.find_all("li")
+        for job in jobs:
+            header = job.find("div", class_="bjs-jlid__header").find_all("a")
+            title = header[0].text
+            company = header[-1].text
+            desciption = job.find("div", class_="bjs-jlid__description").text
+            pattern = re.compile(r"\n\t\t\t(.+?)\t\t")
+            desciption = pattern.findall(desciption)
+            job_url = header[0]
+            if job_url:
+                job_url = job_url["href"]
+
+            job_data = {
+                "title": title,
+                "company": company,
+                "desciption": desciption,
+                "link": job_url,
+            }
+            all_jobs.append(job_data)
 
 # extract job by skill
 def scrap_jobs_by_skill(url, skill):
     by_skill = f"skill-areas/{skill}/"
     scrap_page(url + by_skill)
+
 
 for page in range(get_pages(url)):
     scrap_page(url + f"engineering/page/{page + 1}/")
